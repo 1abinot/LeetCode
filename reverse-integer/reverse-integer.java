aa4757122c95fbf94d03f704d175c70527ddbf3d
1abinot <49006641+1abinot@users.noreply.github.com>
2021-07-15 17:06:42 +0200

    Time: 1 ms (100.00%), Space: 36.3 MB (45.64%) - LeetHub

--- reverse-integer/reverse-integer.java
+++ reverse-integer/reverse-integer.java
@@ -1,15 +1,20 @@
 class Solution {
     public int reverse(int x) {
         int rev=0;
-        
+        int check_overflow=0;
         while(x != 0){
-            if(rev>Integer.MAX_VALUE/10 || rev==Integer.MAX_VALUE/10 && x%10>7)            
-                return 0;
-            if(rev<Integer.MIN_VALUE/10 || rev==Integer.MAX_VALUE/10 && x%10 < -8) 
-                return 0;
+            check_overflow=rev;
             rev = rev * 10 + x % 10;
+            if(check_overflow != rev/10) 
+                return 0;
             x/=10;
         }
         return rev;
     }
 }
+
+/* 
+    if(check_overflow != rev/10) 
+    When the number overflow it will get a random value , so if we divide it by 10 it can never be the same as it was before the overflow!
+*/
+
