ea19b4107ab85bb6bc0a4d8d21c7a2f8768c1d35
1abinot <49006641+1abinot@users.noreply.github.com>
2021-07-23 02:47:15 +0200

    Time: 0 ms (100.00%), Space: 37.3 MB (74.24%) - LeetHub

--- length-of-last-word/length-of-last-word.java
+++ length-of-last-word/length-of-last-word.java
@@ -1,5 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
+        /*
         String[] words = s.split(" ");
         
         if(words.length==0)
@@ -7,5 +8,23 @@ class Solution {
         
         int lastWord = words.length - 1;
         return words[lastWord].length();
+        */
+        
+        
+        int length = 0;
+        
+        if(s.isEmpty())
+            return length;
+        
+        s = s.trim();
+        
+        int n = s.length();
+        for(int i=n-1;i>=0;i--){
+            if(s.charAt(i)==' ')
+                break;
+            length++;
+        }
+        
+        return length;
     }
 }
