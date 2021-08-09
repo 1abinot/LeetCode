0167984f0743e9933a4419a6e59b12bc08e03cca
1abinot <49006641+1abinot@users.noreply.github.com>
2021-08-09 23:12:41 +0200

    Time: 0 ms (100.00%), Space: 37.5 MB (48.88%) - LeetHub

--- plus-one/plus-one.java
+++ plus-one/plus-one.java
@@ -1,5 +1,8 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+        
+        // I solution
+        /*
         int lastIndex = digits.length - 1;
         
         digits[lastIndex] += 1;
@@ -20,6 +23,29 @@ class Solution {
             
             digits = resize;
         }
+        
+        return digits;
+        */
+        
+        
+        // 2 Solution
+        
+        int n = digits.length;
+        
+        for(int i = n-1; i >= 0; --i){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int [] resize = new int[n+1];
+        
+        for(int i=0;i<n;i++){
+            resize[i+1] = digits[i];
+        }
+        resize[0] = 1;
+        digits = resize;
         
         return digits;
     }
